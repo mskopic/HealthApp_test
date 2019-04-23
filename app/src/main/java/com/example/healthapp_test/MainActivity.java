b90d5344a40c1537532e7f8524a57ab48939849b
Sandhya Piratla <sandhya.piratla@gmail.com>
2019-04-22 23:03:34 -0400

    bug fix

--- app/src/main/java/com/example/healthapp_test/MainActivity.java
+++ app/src/main/java/com/example/healthapp_test/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences user = getSharedPreferences("user_details",Context.MODE_PRIVATE);
         SharedPreferences.Editor userEdit = user.edit();
-        userEdit.clear().commit();
         Gson gson = new Gson();
         String s = user.getString("credentials", "null");
         if(s.equals("null")){
